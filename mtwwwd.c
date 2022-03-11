13a926f2170b120f471d680cc7370f6bebb72054
Arran Ø. K. Gabriel <arrankg@gmail.com>
2022-03-11 11:49:22 +0100

    Add htons

--- mtwwwd.c
+++ mtwwwd.c
@@ -3,6 +3,12 @@
 #include <sys/socket.h>
 #include <linux/in.h>
 #include <unistd.h>
+#include <strings.h>
+#include <arpa/inet.h>
+
+#define MAXREQ (4096 * 1024)
+
+char buffer[MAXREQ], body[MAXREQ], msg[MAXREQ];
 
 void error(const char *msg)
 {
@@ -20,32 +26,33 @@ int main(int argc, char *argv[])
 
     char *name = argv[1];
     int port = atoi(argv[2]);
-    int server_fd, new_socket;
-    char buffer[1024] = {0};
+    int socket_fd, new_socket_fd;
+    socklen_t clilen;
 
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    struct sockaddr_in serv_addr, cli_addr;
+    int addrlen = sizeof(serv_addr);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (server_fd == 0)
+    if (socket_fd == 0)
         error("socket failed");
 
-    address.sin_family = AF_INET;
-    // What is this? figure it out
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = port;
+    bzero((char *)&serv_addr, addrlen);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if (bind(socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("bind failed");
 
-    if (listen(server_fd, 1) < 0)
+    if (listen(socket_fd, 1) < 0)
         error("listen failed");
 
-    if (new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen) < 0)
+    if (new_socket_fd = accept(socket_fd, (struct sockaddr *)&serv_addr, (socklen_t *)&addrlen) < 0)
         error("accept failed");
 
-    int readval = read(new_socket, buffer, sizeof(buffer));
+    int readval = read(new_socket_fd, buffer, sizeof(buffer));
     printf("%s\n", buffer);
     return 0;
 }
